40838f785e2d821e4faa533ec7882c4e4066c172
Андрей <andybro17@yandex.ru>
2022-08-12 16:39:27 +0300

    Домашнее задание сдал!

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,7 +1,43 @@
 package ru.skypro;
 
+import ru.skypro.exceptions.WrongLoginException;
+import ru.skypro.exceptions.WrongPasswordException;
+
 public class Main {
 
     public static void main(String[] args) {
+        loginAndPasswordCheck();
+    }
+
+    public static void signInAccountCheck() {
+        String login = "java_skyprollll.";
+        String password = "D_1hWiKjjP_9";
+        String confirmPassword = "D_1hWiKjjP_9";
+
+        if (login.length() > 20) {
+            throw new WrongLoginException();
+        } else if (login.matches("^\\w{1,20}$")) {
+            System.out.println("Логин соответствует требованиям");
+        } else {
+            System.out.println("Логин не соответствует требованиям");
+        }
+
+        if (password.length() != confirmPassword.length()) {
+            throw new WrongPasswordException();
+        } else if (password.equals(confirmPassword) && password.matches("^\\w{1,19}$")) {
+            System.out.println("Пароль соответствует требованиям");
+        } else {
+            throw new WrongPasswordException();
+        }
+    }
+
+    public static void loginAndPasswordCheck() {
+        try {
+            signInAccountCheck();
+        } catch (WrongLoginException e) {
+            System.out.println("Длина логина не может быть больше 20 символов");
+        } catch (WrongPasswordException e) {
+            System.out.println("Пароли не совпадают");
+        }
     }
 }
---
+++ src/ru/skypro/exceptions/WrongLoginException.java
@@ -0,0 +1,4 @@
+package ru.skypro.exceptions;
+
+public class WrongLoginException extends RuntimeException {
+}
